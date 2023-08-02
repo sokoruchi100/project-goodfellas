f65bf0d70006dd65a43c944c8393bf9064b01b91
Daniel Ting <harmeyforever5435@gmail.com>
2023-08-02 13:06:28 -0400

    added picture on screen

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 function Dashboard() {
   const [videoTitles, setVideoTitles] = useState([]);
+  const [selectedImage, setSelectedImage] = useState(null);
 
   useEffect(() => {
     fetch("/api/videos")
@@ -9,6 +10,18 @@ function Dashboard() {
       .then((data) => setVideoTitles(data))
       .catch((error) => console.error("Error fetching video titles:", error));
   }, []);
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      // You can use FileReader to read the selected image and display it on the screen
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setSelectedImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
 
   return (
     <div>
@@ -18,6 +31,20 @@ function Dashboard() {
           <li key={index}>{title}</li>
         ))}
       </ul>
+
+      {/* Button to trigger image selection*/}
+      <input type="file" accept="image/*" onChange={handleImageChange} />
+      {/* Display selected Image */}
+      {selectedImage && (
+        <div>
+          <h2>Selected Profile Picture:</h2>
+          <img
+            src={selectedImage}
+            alt="Selected Profile"
+            style={{ maxWidth: "200px" }}
+          />
+        </div>
+      )}
     </div>
   );
 }
