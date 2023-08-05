4827ef71962864b690dc99c715009dfc5a09d7e3
bashirhilaly <bhilaly@draftstudios.io>
2023-08-04 20:38:09 -0400

    Fixed ieRoutes.js

---
+++ routes/ieRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const passportSetup = require("../passport-setup"); // Import the passport setup file
+const engineController = require("../controllers/authController");
+
+module.exports = router;
+
+
