98b15fd42548a8907012869569c895a7f24de022
Daniel Ting <harmeyforever5435@gmail.com>
2023-08-15 02:20:57 -0400

    Sign Up page done

--- client/src/components/GoogleLoginButton.js
+++ client/src/components/GoogleLoginButton.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import faGoogle from "@fortawesome/free-solid-svg-icons";
 
 const GoogleLoginButton = ({ onClick }) => {
-  return <button onClick={onClick}>Log in with Google</button>;
+  return (
+    <button
+      className="mt-32 w-3/4 flex flex-row items-center bg-white py-4 px-16 rounded-md border-2 border-black"
+      onClick={onClick}
+    >
+      <img
+        className="h-10 w-10 mr-8"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/882px-Google_%22G%22_Logo.svg.png?20230305195327"
+      ></img>
+      <span>Log in with Google</span>
+    </button>
+  );
 };
 
 export default GoogleLoginButton;
